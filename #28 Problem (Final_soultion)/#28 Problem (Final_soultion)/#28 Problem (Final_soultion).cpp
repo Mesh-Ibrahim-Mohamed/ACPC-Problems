ab3b84470ef2751e763b6d3f80d0685a8fd527f1
ibrahim <ib2007a@gmail.com>
2026-03-17 02:53:23 +0200

    Add VS projects, solutions and build outputs
    Add multiple Visual Studio solutions and projects with C++ source files for several problems (e.g. #28 Problem (Final_soultion), #28 Problem, #29 Problem, A. Problem 1, G. Problem 7, H. Problem 8). Includes .vcxproj/.vcxproj.filters/.vcxproj.user, .slnx solution files, .vs workspace metadata and numerous x64/Debug build artifacts (exe, obj, pdb, ilk, tlog, recipe, etc.).

---
+++ #28 Problem (Final_soultion)/#28 Problem (Final_soultion)/#28 Problem (Final_soultion).cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+enum enOddOrEven { Odd = 1, Even = 2};
+
+
+int ReadNumber()
+{
+	int N;
+
+	cout << " Enter Number : ";
+	cin >> N;
+	
+	return N;
+
+}
+
+
+enOddOrEven CheckOddOrEven(int Number)
+{
+	if (Number % 2 != 0)
+		return enOddOrEven::Odd;
+	else
+		return enOddOrEven::Even;
+}
+
+int SumOddNumbersFrom1ToN_UsingWhile(int N)
+{
+	int Sum = 0, i = 1;
+
+	while (N >= i)
+	{
+		if (CheckOddOrEven(i) == enOddOrEven::Odd)
+		{
+			Sum += i;
+		}
+		i++;
+	}
+
+	cout << "The odd Number  By While Loop= " << Sum << endl;
+
+	return Sum;
+
+}
+
+int SumOddNumbersFrom1ToN_UsingDoWhile(int N)
+{
+	int Sum = 0,i = 1;
+
+	if (N <= 0) return 0;
+
+	do
+	{
+		if (CheckOddOrEven(i) == enOddOrEven::Odd)
+		{
+			Sum += i;
+		}
+		i++;
+
+	} while (N >= i);
+
+	cout << "The Odd Number By Do While Do Loop = " << Sum << endl;
+
+	return Sum;
+
+}
+int SumOddNumbersFrom1ToN_UsingForLoop(int n)
+{
+	int sum = 0;
+	
+	for (int i = 0; i <= n; i++)
+	{
+		{
+			if (CheckOddOrEven(i) == enOddOrEven::Odd)
+			{
+				sum += i;
+			}
+		}
+	}
+
+	cout << "The Odd Numbers is = " << sum << endl;
+
+	return sum;
+}
+
+
+
+int main()
+{
+	int n;
+
+	n = ReadNumber();
+
+	SumOddNumbersFrom1ToN_UsingWhile(n);
+	SumOddNumbersFrom1ToN_UsingDoWhile(n);
+	SumOddNumbersFrom1ToN_UsingForLoop(n);
+	return 0;
+}
+
---
+++ H. Problem 8/H. Problem 8/H. Problem 8.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, m;
+	
+	long long sum = 0;
+
+	do {
+		cin >> n >> m;
+
+		sum = 0;
+
+
+		if (n >= m && n > 0 && m > 0)
+		{
+			for (int i = m; i <= n; i++)
+			{
+				cout << i << " ";
+				sum += i;
+
+
+			}
+
+			cout << "sum =" << sum << endl;
+
+		}
+
+		if (n < m && n > 0 && m > 0)
+		{
+			for (int i = n; i <= m; i++)
+			{
+				cout << i << " ";
+				sum += i;
+
+
+			}
+
+			cout << "sum =" << sum << endl;
+		}
+	}
+
+	while (n > 0 && m > 0);
+
+	
+	
+
+
+
+
+
+
+
+
+}
