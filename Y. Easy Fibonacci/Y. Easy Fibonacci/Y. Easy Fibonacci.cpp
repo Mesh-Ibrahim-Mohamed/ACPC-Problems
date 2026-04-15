6dd396e49f4782da9360dae38f1828e661c2b145
ibrahim <ib2007a@gmail.com>
2026-04-15 22:46:37 +0200

    A

---
+++ Y. Easy Fibonacci/Y. Easy Fibonacci/Y. Easy Fibonacci.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N; cin >> N;
+	int arr[100];
+
+	arr[0] = 0; arr[1] = 1;
+
+	for (int i = 2; i < N; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i] << " ";
+
+	}
+	return 0;
+}
---
+++ Z. Three Numbers/Z. Three Numbers/Z. Three Numbers.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+
+	int k, s;
+	cin >> k >> s;
+	int counter = 0;
+
+	for (int i = 999; i <= s; i++)
+	{
+		for (int j = 0; j <= s; j++)
+		{
+			for (int l = 0; l <= s; l++) 
+			{
+				if (i >= 0 && j>=0 && l >= 0 && i<=k && j<=k && l<=k && i+j+l == s){
+					counter++;
+				}
+			}
+		}
+	}
+	cout << counter;
+	return 0;
+}
